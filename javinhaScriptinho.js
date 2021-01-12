db65bc7267ab57a88725c16a829b3ae9fa27dddf
Pedro Valim <pvalim2110@gmail.com>
2021-01-11 21:07:46 -0300

    Site criado com HTML5, CSS Javascript e Bootstrap

---
+++ javinhaScriptinho.js
@@ -0,0 +1,37 @@
+
+
+
+	function calcular_IMC(){
+
+var peso = document.imcForm.peso.value;
+
+var altura = document.imcForm.altura.value;
+
+
+var quadrado = (altura * altura);
+
+
+var calculo = (peso/quadrado);
+
+
+
+if(calculo<18.5){
+alert ("Seu indice IMC é de magreza, " + calculo.toFixed(2) +" acesse a aba de ajuda para saber mais  ");
+}
+if(calculo>=18.5 && calculo<24.9){
+alert("Seu indice IMC é normal, " + calculo.toFixed(2)+" acesse a aba de ajuda para saber mais  ");
+}
+
+if(calculo>=25 && calculo<29.9) {
+alert("Seu indice IMC é de sobrepeso, " + calculo.toFixed(2)+" acesse a aba de ajuda para saber mais  ");
+}
+if(calculo>=30 && calculo<39.9) {
+alert("Seu indice IMC é de obesidade, " + calculo.toFixed(2)+" acesse a aba de ajuda para saber mais  ");
+}
+if (calculo>40)
+alert("Seu indice IMC é de obesidade grave, " + calculo.toFixed(2)+" acesse a aba de ajuda para saber mais  ");
+}
+
+				
+			
+
