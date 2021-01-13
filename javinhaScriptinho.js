f3526c652e29e1e67fb1145961b764ccffc41061
Pedro Valim <pvalim2110@gmail.com>
2021-01-13 00:45:37 -0300

    Redes sociais adicionadas

--- javinhaScriptinho.js
+++ javinhaScriptinho.js
@@ -16,20 +16,20 @@ var calculo = (peso/quadrado);
 
 
 if(calculo<18.5){
-alert ("Seu indice IMC é de magreza, " + calculo.toFixed(2) +" acesse a aba de ajuda para saber mais  ");
+alert ("Seu indice IMC é de magreza " + calculo.toFixed(2));
 }
 if(calculo>=18.5 && calculo<24.9){
-alert("Seu indice IMC é normal, " + calculo.toFixed(2)+" acesse a aba de ajuda para saber mais  ");
+alert("Seu indice IMC é normal " + calculo.toFixed(2));
 }
 
 if(calculo>=25 && calculo<29.9) {
-alert("Seu indice IMC é de sobrepeso, " + calculo.toFixed(2)+" acesse a aba de ajuda para saber mais  ");
+alert("Seu indice IMC é de sobrepeso " + calculo.toFixed(2));
 }
 if(calculo>=30 && calculo<39.9) {
-alert("Seu indice IMC é de obesidade, " + calculo.toFixed(2)+" acesse a aba de ajuda para saber mais  ");
+alert("Seu indice IMC é de obesidade " + calculo.toFixed(2));
 }
 if (calculo>40)
-alert("Seu indice IMC é de obesidade grave, " + calculo.toFixed(2)+" acesse a aba de ajuda para saber mais  ");
+alert("Seu indice IMC é de obesidade grave " + calculo.toFixed(2));
 }
 
 				
